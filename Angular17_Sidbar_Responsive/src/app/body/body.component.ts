fe5e7357797e53d151d6ce0bc8973d08416a18b7
paulo henique <paulohenriquephb29@gmail.com>
2024-04-13 21:57:52 -0300

    add body

--- Angular17_Sidbar_Responsive/src/app/body/body.component.ts
+++ Angular17_Sidbar_Responsive/src/app/body/body.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-body',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './body.component.scss'
 })
 export class BodyComponent {
+@Input() screenWidth=0;
+@Input() collapsed = false;
 
+  getBodyClass(): string {
+    let styleClass = '';
+    if(this.collapsed && this.screenWidth > 768) {
+      styleClass = 'body-trimmed';
+    } else if(this.collapsed && this.screenWidth <= 768 && this.screenWidth > 0) {
+      styleClass = 'body-md-screen'
+    }
+    return styleClass;
+  }
 }
--- Angular17_Sidbar_Responsive/src/app/sidenav/sidenav.component.ts
+++ Angular17_Sidbar_Responsive/src/app/sidenav/sidenav.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Output,EventEmitter } from '@angular/core';
 import { navbarData } from './nav-data';
+
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'app-sidenav',
@@ -7,15 +13,19 @@ import { navbarData } from './nav-data';
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
-
-  collapse = false;
+ @Output() ontoggleCollapse: EventEmitter<SideNavToggle> = new EventEmitter();
+  screenWidth=0;
+  collapsed = false;
   navData = navbarData;
 
   toggleCollapse(): void {
-    this.collapse = !this.collapse;
+    this.collapsed = !this.collapsed;
+  this.ontoggleCollapse.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
   }
 
   closeSidenav() {
-    this.collapse = false;
+    this.collapsed = false;
+  this.ontoggleCollapse.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+
   }
 }
