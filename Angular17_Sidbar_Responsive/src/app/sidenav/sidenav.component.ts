07e8ba2e04593e3e4214a652cfd6f31a7ce3e4a5
paulo henique <paulohenriquephb29@gmail.com>
2024-04-13 22:47:59 -0300

    adionado  responsivo para todso componentes

--- Angular17_Sidbar_Responsive/src/app/sidenav/sidenav.component.ts
+++ Angular17_Sidbar_Responsive/src/app/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
-import { Component, Output,EventEmitter } from '@angular/core';
+import { Component, Output,EventEmitter, OnInit, HostListener } from '@angular/core';
 import { navbarData } from './nav-data';
+
 
 
 interface SideNavToggle {
@@ -12,9 +13,23 @@ interface SideNavToggle {
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit{
+@HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth <= 768 ) {
+      this.collapsed = false;
+      this.ontoggleCollapse.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+    }
+  }
+
+ ngOnInit(): void {
+  if (typeof window !== 'undefined') {
+    this.screenWidth = window.innerWidth;
+  }
+ }
  @Output() ontoggleCollapse: EventEmitter<SideNavToggle> = new EventEmitter();
-  screenWidth=0;
+  screenWidth = 0;
   collapsed = false;
   navData = navbarData;
 
